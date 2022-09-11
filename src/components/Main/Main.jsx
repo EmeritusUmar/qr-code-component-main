0dcfe4fb843f4fe7cc7424c7714e398057a50253
Umar Musa <oladele7532@gmail.com>
2022-09-11 21:30:24 +0100

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ import Card from '../Card/Card';
 function Main() {
   return (
     <div>
-      <div className='flex justify-center items-center h-screen'>
+      <div className='flex justify-center items-center h-[100vh]'>
         <div className='bg-[#D5E1EF] w-[1300px] md:h-[724px] h-full left-[70px] top-[166px] drop-shadow-lg'>
           <div className='flex justify-center h-full items-center'>
             <Card/>
@@ -14,7 +14,7 @@ function Main() {
         </div>
       </div>
 
-        <div className="md:block text-center  hidden">
+        <div className="md:block text-center  hidden mb-3">
       Challenge by <a className="underline font-bold" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
       Coded by <a className="underline font-bold" href="#">Umar Musa</a>.
         </div>
