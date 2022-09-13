f04b820fc6a25aab8afa4db3a000ed3055544aed
Umar Musa <oladele7532@gmail.com>
2022-09-13 11:20:24 +0100

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
   return (
     <div>
       <div className='flex justify-center items-center h-[100vh]'>
-        <div className='bg-[#D5E1EF] w-[1300px] md:h-[724px] h-full left-[70px] top-[166px] drop-shadow-lg'>
+        <div className='bg-[#D5E1EF] w-[1400px] md:h-[724px] h-full left-[70px] top-[166px] drop-shadow-lg'>
           <div className='flex justify-center h-full items-center'>
             <Card/>
           </div>
@@ -16,7 +16,7 @@ function Main() {
 
         <div className="md:block text-center  hidden mb-3">
       Challenge by <a className="underline font-bold" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a className="underline font-bold" href="#">Umar Musa</a>.
+      Coded by <a className="underline font-bold" href="https://umarmusa.netlify.app/" target="_blank" >Umar Musa</a>.
         </div>
     </div>
     
